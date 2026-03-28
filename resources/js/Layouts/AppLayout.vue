4ba450d5079c883a99499927f72572a4485dbddb
bhuvanesha <bhuvneshanand28@gmail.com>
2026-03-28 17:32:27 +0530

    Initial commit - Laravel 12 + Vue + Inertia setup

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nav class="bg-gray-800 text-white p-4">
+      <h1>My App</h1>
+    </nav>
+
+    <main class="p-6">
+      <slot />
+    </main>
+  </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppLayout from '../Layouts/AppLayout.vue';
+
+defineProps({
+  message: String
+});
+</script>
+
+<template>
+  <AppLayout>
+    <h1 class="text-2xl">Home Page</h1>
+    <p>{{ message }}</p>
+  </AppLayout>
+</template>
